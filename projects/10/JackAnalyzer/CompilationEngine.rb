71e92816a12e94111aa11f35d01b523c13c01def
ycl <jeffrey.y.c.liu@gmail.com>
2023-06-11 23:29:26 +0800

    feat: implement peak method

--- projects/10/JackAnalyzer/CompilationEngine.rb
+++ projects/10/JackAnalyzer/CompilationEngine.rb
@@ -11,10 +11,11 @@ class CompilationEngine
     @indentation = 0
   end
 
-  def current_token
-    raise IncorrectTokenIndex if @token_index > (@tokens.size - 1)
+  def peak(peak_index)
+    target_index = @token_index + peak_index
+    raise IncorrectTokenIndex if target_index > (@tokens.size - 1)
 
-    match_data = /<\/?([A-Za-z]+)>(?: (.+) <\/[A-Za-z]+>)?/.match(@tokens[@token_index]).captures
+    match_data = /<\/?([A-Za-z]+)>(?: (.+) <\/[A-Za-z]+>)?/.match(@tokens[target_index]).captures
     { type: match_data[0], value: match_data[1] }
   end
 end
