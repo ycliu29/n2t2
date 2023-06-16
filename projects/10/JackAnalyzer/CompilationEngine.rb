8e92ab34493ad8a1644d3049266e0f3662f48c16
ycl <jeffrey.y.c.liu@gmail.com>
2023-06-16 23:39:37 +0800

    implemented helper methods

--- projects/10/JackAnalyzer/CompilationEngine.rb
+++ projects/10/JackAnalyzer/CompilationEngine.rb
@@ -1,16 +1,30 @@
 require 'pry'
 
 class CompilationEngine
-  attr_accessor :tokens, :token_index, :indentation
+  attr_accessor :tokens, :token_index, :indentation, :output
 
   class IncorrectPeakIndex < StandardError; end
   class IncorrectTokenIndex < StandardError; end
+  class OutOfRangeLastToken < StandardError; end
+  class UnexpectedToken < StandardError; end
 
   def initialize(token_file_path)
     @tokens = IO.readlines(token_file_path)
     @token_index = 0
     @indentation = 0
     @output = File.open("#{File.dirname(token_file_path)}/#{File.basename(token_file_path, ".*")[0..-2]}_tt.xml", 'w')
+  end
+
+  def compile_class
+    eat('tokens', nil)
+    @output.write("<class>")
+    add_linebreak
+
+    @indentation += 2
+    eat('keyword', 'class')
+    indent
+    write_last_token
+    add_linebreak
   end
 
   def peak(peak_index)
@@ -22,6 +36,25 @@ class CompilationEngine
     { type: match_data[0], value: match_data[1] }
   end
 
-  def eat()
+  def eat(type, value)
+    current_token = peak(0)
+
+    riase UnexpectedToken if type != current_token[:type] || value != current_token[:value]
+    @token_index += 1
+
+    current_token
+  end
+
+  def indent
+    @output.write("#{" " * @indentation}")
+  end
+
+  def add_linebreak
+    @output.write("\r\n")
+  end
+
+  def write_last_token
+    raise OutOfRangeLastToken if @token_index == 0
+    @output.write("#{@tokens[@token_index-1]}")
   end
 end
